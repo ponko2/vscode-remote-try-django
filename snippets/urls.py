d0e2908723e1ae3b6d1727afd16bc7adece49404
Takahito Nakano <kano@ponko2.jp>
2024-12-22 21:04:31 +0900

    refactor: Rewriting our API using class-based views
    https://www.django-rest-framework.org/tutorial/3-class-based-views/#rewriting-our-api-using-class-based-views

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,7 +5,7 @@ from snippets import views
 
 urlpatterns = format_suffix_patterns(
     [
-        path("snippets/", views.snippet_list),
-        path("snippets/<int:pk>/", views.snippet_detail),
+        path("snippets/", views.SnippetList.as_view()),
+        path("snippets/<int:pk>/", views.SnippetDetail.as_view()),
     ]
 )
--- snippets/views.py
+++ snippets/views.py
@@ -1,57 +1,60 @@
 from typing import Any, Optional
 
+from django.http import Http404
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-@api_view(["GET", "POST"])
-def snippet_list(request: Request, format: Optional[str] = None) -> Response:
+class SnippetList(APIView):
     """
-    List all code snippets, or create a new snippet.
+    List all snippets, or create a new snippet.
     """
-    if request.method == "GET":
+
+    def get(self, request: Request, format: Optional[str] = None) -> Response:
         snippets = Snippet._default_manager.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    elif request.method == "POST":
+    def post(self, request: Request, format: Optional[str] = None) -> Response:
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-
-@api_view(["GET", "PUT", "DELETE"])
-def snippet_detail(request: Request, pk: Any, format: Optional[str] = None) -> Response:
+class SnippetDetail(APIView):
     """
-    Retrieve, update or delete a code snippet.
+    Retrieve, update or delete a snippet instance.
     """
-    try:
-        snippet = Snippet._default_manager.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == "GET":
+    def get_object(self, pk: Any) -> Snippet:
+        try:
+            return Snippet._default_manager.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request: Request, pk: Any, format: Optional[str] = None) -> Response:
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == "PUT":
+    def put(self, request: Request, pk: Any, format: Optional[str] = None) -> Response:
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
+    def delete(
+        self, request: Request, pk: Any, format: Optional[str] = None
+    ) -> Response:
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
