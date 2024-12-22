c1057615412e83b1aabc5aaabe80b9f708df14e2
Takahito Nakano <kano@ponko2.jp>
2024-12-22 21:31:20 +0900

    chore: Associating Snippets with Users
    https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#associating-snippets-with-users

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework.serializers import BaseSerializer
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
@@ -8,6 +9,9 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 class SnippetList(generics.ListCreateAPIView[Snippet]):
     queryset = Snippet._default_manager.all()
     serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer: BaseSerializer[Snippet]) -> None:
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView[Snippet]):
