a574df60ac9b1da2a679c1f99ef4323377d5cc2b
Takahito Nakano <kano@ponko2.jp>
2024-12-22 21:23:46 +0900

    chore: Adding information to our model
    https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#adding-information-to-our-model

--- snippets/models.py
+++ snippets/models.py
@@ -1,6 +1,10 @@
+from typing import Any
+
 from django.db import models
 from django_stubs_ext.db.models import TypedModelMeta
-from pygments.lexers import get_all_lexers
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -17,6 +21,26 @@ class Snippet(models.Model):
         choices=LANGUAGE_CHOICES, default="python", max_length=100
     )
     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
+    owner = models.ForeignKey(
+        "auth.User", related_name="snippets", on_delete=models.CASCADE
+    )
+    highlighted = models.TextField()
 
     class Meta(TypedModelMeta):
         ordering = ["created"]
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        """Use the `pygments` library to create a highlighted HTML representation of the code snippet."""
+        lexer = get_lexer_by_name(self.language)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style,
+            linenos=linenos,
+            full=True,
+            encoding=None,
+            outencoding=None,
+            **options,
+        )
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
