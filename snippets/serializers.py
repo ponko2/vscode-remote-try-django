c84f02bdc1cf2d151455efabf4221064965db3db
Takahito Nakano <kano@ponko2.jp>
2024-12-22 21:28:41 +0900

    chore: Adding endpoints for our User models
    https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#adding-endpoints-for-our-user-models

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from snippets.models import Snippet
@@ -7,3 +8,13 @@ class SnippetSerializer(serializers.ModelSerializer[Snippet]):
     class Meta:
         model = Snippet
         fields = ["id", "title", "code", "linenos", "language", "style"]
+
+
+class UserSerializer(serializers.ModelSerializer[User]):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet._default_manager.all()
+    )
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "snippets"]
