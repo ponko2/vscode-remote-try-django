3ee82096c7cf650e29c86b7532c1637c6bc31501
Takahito Nakano <kano@ponko2.jp>
2024-12-22 21:28:41 +0900

    chore: Adding endpoints for our User models
    https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#adding-endpoints-for-our-user-models

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,5 +7,7 @@ urlpatterns = format_suffix_patterns(
     [
         path("snippets/", views.SnippetList.as_view()),
         path("snippets/<int:pk>/", views.SnippetDetail.as_view()),
+        path("users/", views.UserList.as_view()),
+        path("users/<int:pk>/", views.UserDetail.as_view()),
     ]
 )
--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import User
 from rest_framework import generics
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView[Snippet]):
@@ -12,3 +13,13 @@ class SnippetList(generics.ListCreateAPIView[Snippet]):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView[Snippet]):
     queryset = Snippet._default_manager.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView[User]):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView[User]):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
