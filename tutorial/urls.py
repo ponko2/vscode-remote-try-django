303a616471e3ed1d2fa502f2cc2b2cc1e1c6360d
Takahito Nakano <kano@ponko2.jp>
2024-12-22 21:35:18 +0900

    chore: Adding login to the Browsable API
    https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#adding-login-to-the-browsable-api

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -23,4 +23,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("snippets.urls")),
+    path("api/v1/auth/", include("rest_framework.urls")),
 ]
