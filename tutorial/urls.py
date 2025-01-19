1a87f5e5b905c72e29241f2b893f2dbdd38becaf
Takahito Nakano <kano@ponko2.jp>
2025-01-19 20:34:15 +0900

    chore: Create the API
    https://django-ninja.dev/tutorial/

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -18,8 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from .api import api
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("snippets.urls")),
     path("api/v1/auth/", include("rest_framework.urls")),
+    path("api/v2/", api.urls),
 ]
